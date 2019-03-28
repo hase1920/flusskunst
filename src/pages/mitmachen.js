75126e7d28907f6784796beecda97881ab6b19e5
hase1920 <48647597+hase1920@users.noreply.github.com>
2019-03-28 10:55:06 +0100

    Update mitmachen.js

--- src/pages/mitmachen.js
+++ src/pages/mitmachen.js
@@ -19,11 +19,11 @@ const Mitmachen =  (props) =>{
  <li style={{borderTop:'1px grey solid',borderBottom:'1px grey solid',display:'block',fontSize:'1.3rem',fontWeight:'400'}}>&rarr; Künstler</li>
 
 <p>
-Herzlich willkommen bei rhein-Kultur, dem netzwerk für Künstler aus der region oberes mittelrheintal! sie wollen bei uns mitmachen?
-dazu schicken sie einfach eine email mit Betreff: Mitmachen an obige Adresse
+Herzlich willkommen bei rhein-Kultur, dem Netzwerk für Künstler aus der Region oberes Mittelrheintal! sie wollen bei uns mitmachen?
+Dazu schicken sie einfach eine Email mit Betreff: Mitmachen an obige Adresse
 Bitte nennen Sie darin:
-Ihren namen, Ihre Kontaktdaten, die Kunstform, die sie ausüben, und fügen ein aussagekräftiges Bild (größe max. 2 mB) hinzu. Wir verarbeiten schnellstmöglich Ihren teilnahmeantrag.
-Alle von Ihnen genannten persönlichen daten werden selbstverständlich streng vertraulich behandelt und nicht an dritte weitergegeben.
+Ihren Namen, Ihre Kontaktdaten, die Kunstform, die sie ausüben, und fügen ein aussagekräftiges Bild (größe max. 2 mB) hinzu. Wir verarbeiten schnellstmöglich Ihren Teilnahmeantrag.
+Alle von Ihnen genannten persönlichen Daten werden selbstverständlich streng vertraulich behandelt und nicht an Dritte weitergegeben.
 
 </p>
 <a href="/portrait.pdf">Portrait: pdf-download</a>
@@ -33,11 +33,10 @@ Alle von Ihnen genannten persönlichen daten werden selbstverständlich streng
 <li style={{borderBottom:'1px grey solid',borderTop:'1px grey solid',display:'block',fontWeight:'400',fontSize:'1.3rem'}}>&rarr; Raumanbieter</li>
 
     <p>
-    Herzlich willkommen bei rhein-Kultur, dem netzwerk für Anbieter von temporä- ren räumen in der region oberes mittel- rheintal! sie wollen bei uns mitmachen? dazu schicken sie einfach eine email mit Betreff: Mitmachen an obige Adresse
+    Herzlich willkommen bei rhein-Kultur, dem Netzwerk für Anbieter von temporären Räumen in der Region Oberes Mittel-Rheintal! Sie wollen bei uns mitmachen? Dazu schicken sie einfach eine email mit Betreff: Mitmachen an obige Adresse.
 Bitte nennen Sie darin:
-Ihren namen, Ihre Kontaktdaten, die räumlichkeit, die sie anbieten möch-
-ten, und fügen ein aussagekräftiges Bild (größe max. 2 mB) hinzu. Wir melden uns schnellstmöglich bei Ihnen, um notwen- dige details zu besprechen und einen Besichtigungstermin zu vereinbaren.
-Alle von Ihnen genannten persönlichen daten werden selbstverständlich streng vertraulich behandelt und nicht an dritte weitergegeben.
+Ihren Namen, Ihre Kontaktdaten, die Räumlichkeit, die sie anbieten möchten, und fügen ein aussagekräftiges Bild (größe max. 2 mB) hinzu. Wir melden uns schnellstmöglich bei Ihnen, um notwendige Details zu besprechen und einen Besichtigungstermin zu vereinbaren.
+Alle von Ihnen genannten persönlichen Daten werden selbstverständlich streng vertraulich behandelt und nicht an Dritte weitergegeben.
 </p>
 <a href="/raumangebot.pdf">Raumangebotsformular: pdf-download</a>
 </Mitmacher>
