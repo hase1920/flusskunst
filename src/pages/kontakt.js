f4b3f0fb3dfff7c12b219f946acb6c1afacb7398
hase1920 <48647597+hase1920@users.noreply.github.com>
2019-04-10 21:15:04 +0200

    Update kontakt.js

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { rhythm } from "../utils/typography"
 import {Link,graphql} from 'gatsby'
-import Layout from '../components/layout
+import Layout from '../components/layout'
 
 const Kontakt = (props) =>{
  const title= props.data.site.siteMetadata.title
