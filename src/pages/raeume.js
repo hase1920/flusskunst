68ea7b36141bc168e6744fb1a00ef536062186ae
hase1920 <48647597+hase1920@users.noreply.github.com>
2019-04-10 21:17:31 +0200

    Update raeume.js

--- src/pages/raeume.js
+++ src/pages/raeume.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import Layout from '../components/layout'
+
 class Raum extends React.Component {
 
     render(){
+      const title = props.data.site.siteMetadata.title
         return(
-            <Layout location= {this.props.location} >            <div>
-
-            
+            <Layout location={props.location} title={title}>
+         
             <h1> Hier stehen Raumnachfragen oder -Angebote</h1>
             <a href="/Rhein-Kultur_Layout_Raeume.pdf"> -
             &rarr; So wird die Raumseite aussehen &rarr; PDF öffnen</a>
-            </div>
+           
             </Layout>
 
         )
     }
 }
 export default Raum
+
+export const raumQuery = graphql`
+  query  {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+  }`
