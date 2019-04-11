4116929a08ff445794de1b9392bc4d12ab77354e
hase1920 <48647597+hase1920@users.noreply.github.com>
2019-04-11 07:44:38 +0200

    Update datenschutz.js

--- src/pages/datenschutz.js
+++ src/pages/datenschutz.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { rhythm} from "../utils/typography"
-import {Link,graphql} from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../components/layout'
 
 const Datenschutz = (props) => {
