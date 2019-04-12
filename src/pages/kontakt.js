8f0b8b8bc3da17e87e62c87e90fe8ca3b879d9a8
hase1920 <48647597+hase1920@users.noreply.github.com>
2019-04-12 22:12:25 +0200

    Update kontakt.js

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 import Layout from '../components/layout'
+import {Blogdiv2} from '../components/format'
 
 const Kontakt = (props) =>{
  const title= props.data.site.siteMetadata.title
  return(
  <Layout location={props.location} title={title}>
-
+<Blogdiv2>
 <h2>Kontakt</h2>
 <p>Hier werden die Kontaktdaten der Bürgermeister und
   der Verwaltungen von Bakaloni stehen.
@@ -15,7 +16,7 @@ const Kontakt = (props) =>{
 René Broich <br />
 Langstr. 48<br />
 55422 Bacharach<br />
-
+</Blogdiv2>
  </Layout>
 )}
 export default Kontakt
