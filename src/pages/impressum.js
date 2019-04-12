7814c91c8d488e56b665239d3e96bfa439c15379
hase1920 <48647597+hase1920@users.noreply.github.com>
2019-04-12 22:16:25 +0200

    Update impressum.js

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import {Link,graphql} from 'gatsby'
 import Layout from '../components/layout'
+import {Blogdiv2} from '../components/format'
 
 const Impressum = (props) => {
   const title = props.data.site.siteMetadata.title
  return (
   <Layout location={props.location} title={title}>
-
+<Blogdiv2>
 <h2>Impressum</h2>
 René Broich <br/>
 Langstr. 48 <br/>
 Email: renbroi@icloud.components <br/>
 <Link to = "/datenschutz"> &rarr; Link zu den Datenschutzbestimmungen</Link><br/>
 Diese Webseite wurde von René Broich programmiert.
-
+</Blogdiv2>
  </Layout>
 
 )}
